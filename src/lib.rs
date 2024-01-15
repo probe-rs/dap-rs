5d63728d2cd1a10dc54d5a78e93efbb27b0f66ec
Dominik Boehi <dboehi@fastmail.com>
2024-01-15 18:32:56 +0100

    Expose USB device

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,7 @@ pub mod usb;
 
 #[cfg(test)]
 mod mock_device;
+
+// Re-export the usb-device crate, so that crates depending on us can use it without
+// having to track it as a separate dependency.
+pub use usb_device;
